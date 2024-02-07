45894b55825df2be37b26e65efbfb485c520a06a
jasoncast880 <jasoncast880@gmail.com>
2024-02-07 16:52:42 -0500

    added basic lsp support

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,7 +1,7 @@
 return {
     "nvim-treesitter/nvim-treesitter", build = ":TSUpdate",
     config = function()
-     
+
         local config = require("nvim-treesitter.configs")
         config.setup({
 		ensure_installed = {"javascript", "c", "cpp", "python", "html", "lua", "dart"},
